601685815c9b0f833a18ade8dda2f2d79b222919
Sinks <125329866+nicksinks@users.noreply.github.com>
2023-02-27 19:18:57 -0500

    Adding NickTest to the Repository

---
+++ src/project_green/NickTest.java
@@ -0,0 +1,13 @@
+package project_green;
+
+public class NickTest {
+    
+    public static void output() {
+        
+        System.out.println("Hello.   I'm still testing GitHub");
+        
+    }
+
+
+
+}
