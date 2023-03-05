79f3f52f3330f039d1e8844462901e732f04d52c
gataroo4 <125308766+gataroo4@users.noreply.github.com>
2023-03-04 21:23:48 -0500

    Update NickTest.java

--- src/project_green/NickTest.java
+++ src/project_green/NickTest.java
@@ -5,6 +5,7 @@ public class NickTest {
     public static void output() {
         
         System.out.println("Hello.   I'm still testing GitHub");
+        System.out.println("Kristy testing, 1, 2");
         
     }
 
