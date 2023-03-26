27b365758cefbb78811a944a4e99c5896457ea40
gataroo4 <125308766+gataroo4@users.noreply.github.com>
2023-03-26 18:52:01 -0400

    Add files via upload

---
+++ Traits.java
@@ -0,0 +1,42 @@
+
+public class Traits {
+    private boolean mood;
+    private boolean mustache;
+    private boolean booty;
+    private boolean bankroll;
+    
+    public  Traits (boolean mood, boolean mustache, boolean booty, boolean bankroll){
+        
+        this.mood = mood;
+        this.mustache =  mustache;
+        this.booty = booty;
+        this.bankroll = bankroll;
+    }
+    
+    public boolean isMood() {
+        return mood;
+    }
+    public void setMood(boolean mood) {
+        this.mood = mood;
+    }
+    public boolean isMustache() {
+        return mustache;
+    }
+    public void setMustache(boolean mustache) {
+        this.mustache = mustache;
+    }
+    public boolean isBooty() {
+        return booty;
+    }
+    public void setBooty(boolean booty) {
+        this.booty = booty;
+    }
+    public boolean isBankroll() {
+        return bankroll;
+    }
+    public void setBankroll(boolean bankroll) {
+        this.bankroll = bankroll;
+    }
+    
+    
+}
