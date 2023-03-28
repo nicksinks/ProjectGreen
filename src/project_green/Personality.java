c88c17a15df965f76d106f69ef7d24d81d04febc
zsykes1122 <126403588+zsykes1122@users.noreply.github.com>
2023-03-28 12:27:10 -0400

    updated personality

--- src/project_green/Personality.java
+++ src/project_green/Personality.java
@@ -1,19 +1,39 @@
 
-public abstract  Personality implements Comparable<Person> {
+public abstract Personality implements Comparable<Person> {
 	
+
 	public person() {
 		
 	}
-}
-
-  public abstract Class Personality()
-{
-	String name;
-	boolean[] compositTraites;
 	
-	getName();
-	setName();  
+	public String getName() 
+	{
+		return name;
+	}
 	
-	getPersonalityProfile (compositeTraits);
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
 	
+	public boolean[] getPersonalityProfile() 
+	{
+		return personalityProfile;
+	}
+	
+	public void setPersonalityProfile(boolean[] personalityProfile) 
+	{
+		this.personalityProfile = personalityProfile;
+	}
+	
+	public int getPersonalityScore() 
+	{
+		return personalityScore;
+	}
+	
+	public void setPersonalityScore(int personalityScore) 
+	{
+		this.personalityScore = personalityScore;
+		
+	}
 }
