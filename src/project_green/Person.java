b9fb467a85d63fd51d4656504b5d1a51965aab8f
Sinks <125329866+nicksinks@users.noreply.github.com>
2023-03-27 21:15:48 -0400

    updating Person class stub

---
+++ src/project_green/Person.java
@@ -0,0 +1,49 @@
+package project_green;
+
+public class Person  implements Comparable<Person>  {
+    
+        String name;
+        String email;
+        Personality yourPersonality;
+    
+    
+    public Person(String name, String email, Personality yourPersonality) {
+        this.name = name;
+        this.email = email;
+        this.yourPersonality = null;
+        
+    }
+    
+    getName(){
+        
+    }
+    
+    setName(){
+        
+    }
+    
+    getEmail(){
+        
+    }
+    
+    setEmail(){
+        
+    }
+    
+    getPersonality(){
+        
+    }
+    
+    setPersonality(Quiz q, Traits t){
+        
+    }
+    
+    compareTo(Person x){
+        
+        return x
+        
+    }
+    
+}
+
+
