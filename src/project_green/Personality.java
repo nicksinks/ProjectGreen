8d0ddd04eeed58835f8711cce5bad029789f8e2f
zsykes1122 <126403588+zsykes1122@users.noreply.github.com>
2023-03-28 12:42:36 -0400

    Update Personality.java

--- src/project_green/Personality.java
+++ src/project_green/Personality.java
@@ -1,38 +1,49 @@
-
-public abstract Personality implements Comparable<Person> {
+public abstract class Personality implements Comparable<Person> {
+	
+	private String name;
+	private boolean[] personalityProfile;
+	private int personalityScore;
 	
 
-	public person() {
-		
+	public void person() {
+		this.name = "person";
+		this.personalityProfile = new boolean[4];
+		this.personalityScore = 0;
 	}
 	
-	public String getName() 
-	{
+	public void person(String name, boolean[] personalityProfile) {
+		this.name = name;
+		this.personalityProfile = personalityProfile;
+		this.personalityScore = 0;
+	}
+	
+	public void person(String name, boolean[] personalityProfile, int personalityScore) {
+		this.name = name;
+		this.personalityProfile = personalityProfile;
+		this.personalityScore = personalityScore;
+	}
+	
+	public String getName() {
 		return name;
 	}
 	
-	public void setName(String name) 
-	{
+	public void setName(String name) {
 		this.name = name;
 	}
 	
-	public boolean[] getPersonalityProfile() 
-	{
+	public boolean[] getPersonalityProfile() {
 		return personalityProfile;
 	}
 	
-	public void setPersonalityProfile(boolean[] personalityProfile) 
-	{
+	public void setPersonalityProfile(boolean[] personalityProfile) {
 		this.personalityProfile = personalityProfile;
 	}
 	
-	public int getPersonalityScore() 
-	{
+	public int getPersonalityScore() {
 		return personalityScore;
 	}
 	
-	public void setPersonalityScore(int personalityScore) 
-	{
+	public void setPersonalityScore(int personalityScore) {
 		this.personalityScore = personalityScore;
 		
 	}
