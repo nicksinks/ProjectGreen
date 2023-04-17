831c9c8d407669e042dfc8866349f431b6b8eb2c
Sinks <125329866+nicksinks@users.noreply.github.com>
2023-04-16 20:00:51 -0400

    created the GUI base scene

---
+++ src/application/justaGUIController.java
@@ -0,0 +1,26 @@
+package application;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class justaGUIController {
+
+    @FXML
+    private TextField personEmail;
+
+    @FXML
+    private TextField personName;
+
+    @FXML
+    void exit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newQuiz(ActionEvent event) {
+
+    }
+
+}
