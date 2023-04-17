f89250a0b2dbca6a07aea8a826cd6c6df0061945
Sinks <125329866+nicksinks@users.noreply.github.com>
2023-04-17 19:52:39 -0400

    group work on GUI
    Working on GUI items
    
    Co-Authored-By: gataroo4 <125308766+gataroo4@users.noreply.github.com>
    Co-Authored-By: storrijas97 <125398414+storrijas97@users.noreply.github.com>
    Co-Authored-By: zsykes1122 <126403588+zsykes1122@users.noreply.github.com>

--- src/application/justaGUIController.java
+++ src/application/justaGUIController.java
@@ -1,26 +1,42 @@
 package application;
 
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class justaGUIController {
 
     @FXML
     private TextField personEmail;
+       
 
     @FXML
     private TextField personName;
+    
+    @FXML
+    private Label emailLbl;
+
+    @FXML
+    private Label nameLabel;
 
     @FXML
     void exit(ActionEvent event) {
-
+        Platform.exit();
     }
 
     @FXML
     void newQuiz(ActionEvent event) {
-
+        Person person = new Person(personName.getText(), personEmail.getText());
+        Quiz quiz = new Quiz();
+        personEmail.setVisible(false);
+        personName.setVisible(false);
+        emailLbl.setVisible(false);
+        nameLabel.setVisible(false);
+        
+        
     }
 
 }
