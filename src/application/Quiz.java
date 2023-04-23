5fdab60ba4fc018076ca36102b8c23b489f2cc17
gataroo4 <gataroo@yahoo.com>
2023-04-23 19:43:38 -0400

    added yes and no

--- src/application/Quiz.java
+++ src/application/Quiz.java
@@ -6,16 +6,73 @@ import java.util.Scanner;
 public class Quiz {
     
     
-    int moodCounter, mustacheCounter, bootyCounter, bankrollCounter;
+    public int moodCounter, mustacheCounter, bootyCounter, bankrollCounter;
     static Scanner scan = new Scanner(System.in);
 
 
     public Quiz() {
-        // TODO Auto-generated constructor stub
+        this.moodCounter = 0;
+        this.mustacheCounter = 0;
+        this.bootyCounter = 0;
+        this.bankrollCounter = 0;
+       
     }
     
 
     
+    public int getMoodCounter() {
+        return moodCounter;
+        
+    }
+
+
+
+    public void setMoodCounter() {
+        System.out.println("test" + this.moodCounter);
+        
+        this.moodCounter++;
+        
+        System.out.println("test after" + this.moodCounter);
+    }
+
+
+
+    public int getMustacheCounter() {
+        return mustacheCounter;
+    }
+
+
+
+    public void setMustacheCounter(int mustacheCounter) {
+        this.mustacheCounter = mustacheCounter;
+    }
+
+
+
+    public int getBootyCounter() {
+        return bootyCounter;
+    }
+
+
+
+    public void setBootyCounter(int bootyCounter) {
+        this.bootyCounter = bootyCounter;
+    }
+
+
+
+    public int getBankrollCounter() {
+        return bankrollCounter;
+    }
+
+
+
+    public void setBankrollCounter(int bankrollCounter) {
+        this.bankrollCounter = bankrollCounter;
+    }
+
+
+
     public void addPerson() {
         try {
             
@@ -58,50 +115,50 @@ public class Quiz {
         
         
         int [] answerCounter = new int[] {moodCounter,mustacheCounter,bootyCounter,bankrollCounter};
-        setTraitsFromQuizAnswers(answerCounter);
+        //setTraitsFromQuizAnswers(answerCounter);
    
 
     }
     
     
     
-    public Traits setTraitsFromQuizAnswers(int[] x) {
-        //if answers are 2 or 3 of 3, set Traits value to true
-        //if answers are 0 or 1 of 3, set Traits value to false  
-        boolean mood, mustache, booty, bankroll;
-        
-        if (x[0] < 2) {
-            mood = false;
-        } else {
-            mood = true;
-        }
-        
-        if (x[1] < 2) {
-            mustache = false;
-        } else {
-            mustache = true;
-        }
-        if (x[2] < 2) {
-            booty = false;
-        } else {
-            booty = true;
-        }
-        if (x[3] < 2) {
-            bankroll = false;
-        } else {
-            bankroll = true;
-        }
-        
-        
-        
-        Traits yourTraits = new Traits(mood, mustache, booty, bankroll);
-        person.setTraits(yourTraits);
-        return yourTraits; 
-
-        
-        
-        
-    }
+//    public Traits setTraitsFromQuizAnswers(int[] x) {
+//        //if answers are 2 or 3 of 3, set Traits value to true
+//        //if answers are 0 or 1 of 3, set Traits value to false  
+//        boolean mood, mustache, booty, bankroll;
+//        
+//        if (x[0] < 2) {
+//            mood = false;
+//        } else {
+//            mood = true;
+//        }
+//        
+//        if (x[1] < 2) {
+//            mustache = false;
+//        } else {
+//            mustache = true;
+//        }
+//        if (x[2] < 2) {
+//            booty = false;
+//        } else {
+//            booty = true;
+//        }
+//        if (x[3] < 2) {
+//            bankroll = false;
+//        } else {
+//            bankroll = true;
+//        }
+//        
+//        
+//        
+////        Traits yourTraits = new Traits(mood, mustache, booty, bankroll);
+////        person.setTraits(yourTraits);
+////       return yourTraits; 
+//
+//        
+//        
+//        
+//    }
     
     
 
@@ -109,4 +166,6 @@ public class Quiz {
 
         
 }
+
+
 
--- src/application/justaGUIController.java
+++ src/application/justaGUIController.java
@@ -20,6 +20,11 @@ import javafx.stage.Stage;
 
 
 public class justaGUIController {
+    
+    
+    Quiz quiz = new Quiz();
+    
+    
 
     @FXML
     private TextField personEmail;
@@ -67,12 +72,34 @@ public class justaGUIController {
 
     @FXML
     private Label question1Label;
+    
+    @FXML
+    private Label question2Label;
+    @FXML
+    private Label question3Label;
+    @FXML
+    private Label question4Label;
+    @FXML
+    private Label question5Label;
+    @FXML
+    private Label question6Label;
+    @FXML
+    private Label question7Label;
+    @FXML
+    private Label question8Label;
+    @FXML
+    private Label question9Label;
+    @FXML
+    private Label question10Label;
+    @FXML
+    private Label question11Label;
+    @FXML
+    private Label question12Label;
 
 
     @FXML
     void newQuiz(ActionEvent event) throws IOException {
         Person person = new Person(personName.getText(), personEmail.getText());
-        Quiz quiz = new Quiz();
         personEmail.setVisible(false);
         personName.setVisible(false);
         emailLbl.setVisible(false);
@@ -96,6 +123,8 @@ public class justaGUIController {
 //        // no mustache ++
 //        switchToQuestion2(event);
 //    }
+    
+     @FXML
 
     public void switchToQuestion1(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Question1.fxml"));
@@ -104,100 +133,215 @@ public class justaGUIController {
         stage.setScene(scene);
         stage.show();
     }
-    
-    public void switchToQuestion2(ActionEvent event) throws IOException {
+    @FXML
+    public void noSwitchToQuestion2(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Question2.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    } 
+    @FXML
+    public void yesSwitchToQuestion2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Question2.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        quiz.setMustacheCounter(0);
     }
     
-    public void switchToQuestion3(ActionEvent event) throws IOException {
+    @FXML
+    public void yesSwitchToQuestion3(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Question3.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        quiz.setMustacheCounter(0);
+    }
+    @FXML
+    public void noSwitchToQuestion3(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Question3.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
-    public void switchToQuestion4(ActionEvent event) throws IOException {
+    @FXML
+    public void yesSwitchToQuestion4(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Question4.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        quiz.setMustacheCounter(0);
+    }
+    @FXML
+    public void noSwitchToQuestion4(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Question4.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
-    public void switchToQuestion5(ActionEvent event) throws IOException {
+    @FXML
+    public void yesSwitchToQuestion5(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Question5.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        quiz.setMoodCounter();
+    }
+    @FXML
+    public void noSwitchToQuestion5(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Question5.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
-    public void switchToQuestion6(ActionEvent event) throws IOException {
+    @FXML
+    public void yesSwitchToQuestion6(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Question6.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        quiz.setMoodCounter();
+    }
+    @FXML
+    public void noSwitchToQuestion6(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Question6.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
-    public void switchToQuestion7(ActionEvent event) throws IOException {
+    @FXML
+    public void yesSwitchToQuestion7(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Question7.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        quiz.setMoodCounter();
+    }
+    @FXML
+    public void noSwitchToQuestion7(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Question7.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
-    public void switchToQuestion8(ActionEvent event) throws IOException {
+    @FXML
+    public void yesSwitchToQuestion8(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Question8.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        quiz.setBootyCounter(0);
+    }
+    @FXML
+    public void noSwitchToQuestion8(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Question8.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
-    public void switchToQuestion9(ActionEvent event) throws IOException {
+    @FXML
+    public void yesSwitchToQuestion9(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Question9.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        quiz.setBootyCounter(0);
+    }
+    @FXML
+    public void noSwitchToQuestion9(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Question9.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
-    public void switchToQuestion10(ActionEvent event) throws IOException {
+    public void yesSwitchToQuestion10(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Question10.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        quiz.setBootyCounter(0);
+    }
+    @FXML
+    public void noSwitchToQuestion10(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Question10.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
-    public void switchToQuestion11(ActionEvent event) throws IOException {
+    @FXML
+    public void yesSwitchToQuestion11(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Question11.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        quiz.setBankrollCounter(0);
+    }
+    @FXML
+    public void noSwitchToQuestion11(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Question11.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    public void switchToQuestion12(ActionEvent event) throws IOException {
+    @FXML
+    public void yesSwitchToQuestion12(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Question12.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        quiz.setBankrollCounter(0);
+    }
+    @FXML
+    public void noSwitchToQuestion12(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Question12.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    public void switchToProfilePage(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("ProfilePage.fxml"));
+    @FXML
+    public void yesSwitchToProfilePage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("profilePage.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        quiz.setBankrollCounter(0);
     }
+    @FXML
+    public void noSwitchToProfilePage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("profilePage.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        System.out.println(quiz.getMoodCounter());
+    }
+    //
     
-
+    
+  
+    
+    
 }
